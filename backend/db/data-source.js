e589b54f3cb8d34d516e7c12b0fef5f15e830c6a
TzuHan_Chen <hahachentzuhan@gmail.com>
2026-08-24 19:03:13 +0800

    feat: 建立 db/data-source.js

--- backend/db/data-source.js
+++ backend/db/data-source.js
@@ -1,18 +1,25 @@
-require('dotenv').config()
-const { DataSource } = require('typeorm')
+const { DataSource } = require("typeorm");
+const config = require("../config/index");
 
 const dataSource = new DataSource({
-	type: 'postgres',
-	host: process.env.DB_HOST,
-	port: Number(process.env.DB_PORT),
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	synchronize: process.env.DB_SYNCHRONIZE === 'true',
-	ssl: process.env.DB_ENABLE_SSL === 'true',
-	entities: [
-		// 今天的 entities 會一個一個長出來
-	],
-})
+  type: "postgres",
+  host: config.get("db.host"),
+  port: Number(config.get("db.port")),
+  username: config.get("db.username"),
+  password: config.get("db.password"),
+  database: config.get("db.database"),
+  synchronize: config.get("db.synchronize"),
+  ssl: config.get("db.ssl"),
+  entities: [
+    require("../entities/User"),
+    require("../entities/Skill"),
+    require("../entities/Coach"),
+    require("../entities/CoachLinkSkill"),
+    require("../entities/CreditPackage"),
+    require("../entities/CreditPurchase"),
+    require("../entities/Course"),
+    require("../entities/CourseBooking"),
+  ],
+});
 
-module.exports = { dataSource }
+module.exports = { dataSource };
