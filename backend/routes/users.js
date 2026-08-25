094002c4af99ce035bae200d35cca28d58a5db8d
TzuHan_Chen <hahachentzuhan@gmail.com>
2026-08-25 16:31:18 +0800

    feat: 登入

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const usersController = require("../controllers/users");
 
 router.post("/signup", usersController.signup);
+router.post("/login", usersController.login);
 
 module.exports = router;
