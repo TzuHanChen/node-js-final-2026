7ce944f3b1c60f0752a3ac7746b1f3e89dd9edc4
TzuHan_Chen <hahachentzuhan@gmail.com>
2026-08-28 15:06:16 +0800

    feat: 查單一課程、更新課程

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -97,8 +97,8 @@ const adminCoachesController = {
 		}
 		const result = await coachRepo.update(coach.id, {
 			experience_years: experience_years,
-			description: description.trim(),
-			profile_image_url: profile_image_url.trim()
+			description: description,
+			profile_image_url: profile_image_url
 		});
 		if (result.affected === 0) {
 			return next(appError(400, "更新使用者資料失敗"));
@@ -163,8 +163,8 @@ const adminCoachesController = {
 			!meeting_url.startsWith("https")) {
 			return next(appError(400, "欄位未填寫正確"));
 		}
-		const coachRepo = dataSource.getRepository("Course");
-		const course = await coachRepo.save({
+		const courseRepo = dataSource.getRepository("Course");
+		const course = await courseRepo.save({
 			user_id: req.user.id,
 			skill_id: skill_id,
 			name: name,
@@ -179,12 +179,60 @@ const adminCoachesController = {
 	},
 
 	async getAdminCourse(req, res, next) {
-		res.json({ status: "success", data: [] });
+		const course = await dataSource.query(`
+		  SELECT c.id, c.name, c.description, c.start_at, c.end_at, c.max_participants, s.name AS skill_name, c.skill_id, c.meeting_url
+		  FROM "courses" c
+			JOIN "skills" s
+			ON c.skill_id = s.id
+			WHERE c.id = $1 AND c.user_id = $2
+		`, [req.params.courseId, req.user.id]);
+		if (!course || course.length === 0) {
+			return next(appError(400, "課程不存在"));
+		}
+		res.json({ status: "success", data: course[0] });
 		return;
 	},
 
 	async putAdminCourse(req, res, next) {
-		res.json({ status: "success", data: [] });
+		const { skill_id, name, description, start_at, end_at, max_participants, meeting_url } = req.body;
+		if (!isValidString(skill_id) ||
+			!isValidString(name) ||
+			!isValidString(description) ||
+			!isValidString(start_at) ||
+			!isValidString(end_at) ||
+			!isInteger(max_participants) ||
+			max_participants < 0 ||
+			!isValidString(meeting_url) ||
+			!meeting_url.startsWith("https")) {
+			return next(appError(400, "欄位未填寫正確"));
+		}
+		const courseRepo = dataSource.getRepository("Course");
+		const course = await courseRepo.findOne({
+			select: { id: true, user_id: true },
+			where: { id: req.params.courseId, user_id: req.user.id }
+		});
+		if (!course) {
+			return next(appError(400, "課程不存在"));
+		}
+		const result = await courseRepo.update({
+			id: req.params.courseId,
+			user_id: req.user.id
+		}, {
+			skill_id: skill_id,
+			name: name,
+			description: description,
+			start_at: start_at,
+			end_at: end_at,
+			max_participants: max_participants,
+			meeting_url: meeting_url
+		})
+		if (result.affected === 0) {
+			return next(appError(400, "更新課程資料失敗"));
+		}
+		const course_new = await courseRepo.findOneBy({
+			id: req.params.courseId, user_id: req.user.id
+		});
+		res.json({ status: "success", data: { course: course_new } });
 		return;
 	},
 };
